9a8ce9e866bf006d9b74f902d65f3676aee2d052
kimilguk <boramcom@daum.net>
2021-05-08 14:33:01 +0900

    자바스크립트jQuery실습

---
+++ 30/user.js
@@ -0,0 +1,74 @@
+var user = [
+  {
+    "_id": "60962114d8421430abce3340",
+    "age": 25,
+    "eyeColor": "blue",
+    "name": "Esther Lewis"
+  },
+  {
+    "_id": "6096211478a01fc571ba8bea",
+    "age": 33,
+    "eyeColor": "blue",
+    "name": "Blair Sanford"
+  },
+  {
+    "_id": "60962114232929716c7bf7ce",
+    "age": 26,
+    "eyeColor": "green",
+    "name": "Merritt Phelps"
+  },
+  {
+    "_id": "60962114dbeacd26e90f2e46",
+    "age": 33,
+    "eyeColor": "green",
+    "name": "Tara Mills"
+  },
+  {
+    "_id": "60962114e2b23d90b5a26dfc",
+    "age": 31,
+    "eyeColor": "brown",
+    "name": "Rush Sherman"
+  },
+  {
+    "_id": "60962114e8efa80af84f9b04",
+    "age": 38,
+    "eyeColor": "brown",
+    "name": "Arnold Lindsey"
+  },
+  {
+    "_id": "609621146658b041761a5d72",
+    "age": 20,
+    "eyeColor": "brown",
+    "name": "Meredith Ferguson"
+  },
+  {
+    "_id": "6096211494d35bed33e916d4",
+    "age": 28,
+    "eyeColor": "green",
+    "name": "Campbell Mcgee"
+  },
+  {
+    "_id": "60962114f69f4aa76ce902e4",
+    "age": 21,
+    "eyeColor": "green",
+    "name": "Stanley Page"
+  },
+  {
+    "_id": "609621143891f9327ee215d3",
+    "age": 21,
+    "eyeColor": "green",
+    "name": "Jessie Dixon"
+  },
+  {
+    "_id": "60962114b0ddb970946abfc7",
+    "age": 28,
+    "eyeColor": "blue",
+    "name": "Lorena Atkins"
+  },
+  {
+    "_id": "609621149224786e38d8a684",
+    "age": 22,
+    "eyeColor": "green",
+    "name": "Case Hudson"
+  }
+]
